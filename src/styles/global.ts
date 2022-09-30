6dbccbb618fbdd09c659261c5c5d3a2affb2d273
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-09-30 08:33:54 -0300

    Chore: Configure Eslint + Global Styles

---
+++ src/styles/global.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --toastify-color-light: #29292E;
+    --toastify-color-dark: #29292E;
+    --toastify-color-info: linear-gradient(90deg, #39B74A 0%, #113817 100%);
+    --toastify-color-error: linear-gradient(90deg, #319E40 0%, #113817 100%);
+    --toastify-color-warning: linear-gradient(90deg, #319E40 0%, #113817 100%);
+    --toastify-color-success: linear-gradient(90deg, #319E40 0%, #113817 100%);
+    --toastify-icon-color-info: #C4C4CC;
+    --toastify-icon-color-error: #AB222E;
+    --toastify-icon-color-warning: #c47f17;
+    --toastify-icon-color-success: #319E40;  
+  
+    --toastify-text-color-light: #C4C4CC;
+    --toastify-text-color-dark: #C4C4CC;
+  }
+
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+}
+
+  body {
+    background-color: ${(props) => props.theme['gray-200']};
+    color: ${(props) => props.theme['gray-800']};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    
+  }
+
+  body, input-security, textarea, button {
+    font: 400 1rem 'Nunito Sans', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .Toastify__close-button {
+    color: ${(props) => props.theme['gray-300']};
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  'pure-white': '#FFFFFF',
+
+  'gray-100': '#FBFBFB',
+  'gray-200': '#F1F1F1',
+  'gray-300': '#D9D9D9',
+  'gray-400': '#9B9B9B',
+  'gray-500': '#7A7A7A',
+  'gray-700': '#424242',
+  'gray-800': '#0E0E0E',
+  'gray-900': '#070500',
+
+  'yellow-100': '#F4E8D2',
+  'yellow-300': '#F2CB81',
+  'yellow-500': '#F7A407',
+  'yellow-700': '#CD952A',
+  'yellow-800': '#795207',
+
+  'green-300': '#39B74A',
+
+  'red-300': '#F75A68',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+} as const
